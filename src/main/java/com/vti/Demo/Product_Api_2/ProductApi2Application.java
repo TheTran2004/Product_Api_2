ed1018157e2009e2223f01ef8e4fded5095655e9
ANOS_TMT\trant <tranthep363184700@gmail.com>
2023-08-17 13:46:03 +0700

    first

---
+++ src/main/java/com/vti/Demo/Product_Api_2/ProductApi2Application.java
@@ -0,0 +1,13 @@
+package com.vti.Demo.Product_Api_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApi2Application.class, args);
+	}
+
+}
